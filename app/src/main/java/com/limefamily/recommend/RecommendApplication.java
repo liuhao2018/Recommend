81c8a1c1ef64b2bc978385b9f1e924db5e6c5aeb
liuhao <liuhao@limefamily.com>
2018-04-10 14:25:40 +0800

    添加用于测试的公共请求头host

--- app/src/main/java/com/limefamily/recommend/RecommendApplication.java
+++ app/src/main/java/com/limefamily/recommend/RecommendApplication.java
@@ -2,6 +2,13 @@ package com.limefamily.recommend;
 
 import android.app.Application;
 import com.facebook.drawee.backends.pipeline.Fresco;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -14,6 +21,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RecommendApplication extends Application {
 
     private static Retrofit retrofit;
+
+    private final String API_HOST = "http://api.recommend.com";
 
     @Override
     public void onCreate() {
@@ -28,10 +37,31 @@ public class RecommendApplication extends Application {
         retrofit = new Retrofit.Builder()
                 .baseUrl(BuildConfig.API_BASE_ADDRESS)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(generateClient())
                 .build();
     }
 
     public static Retrofit getRetrofit() {
         return retrofit;
+    }
+
+    /**
+     * 添加用于测试的公共请求头
+     * @return
+     */
+    private OkHttpClient generateClient() {
+        OkHttpClient httpClient = new OkHttpClient.Builder()
+                .addInterceptor(new Interceptor() {
+                    @Override
+                    public Response intercept(Chain chain) throws IOException {
+                        Request request = chain.request()
+                                .newBuilder()
+                                .addHeader("Host", API_HOST)
+                                .build();
+                        return chain.proceed(request);
+                    }
+                })
+                .build();
+        return httpClient;
     }
 }
--- app/src/main/java/com/limefamily/recommend/restful/UserService.java
+++ app/src/main/java/com/limefamily/recommend/restful/UserService.java
@@ -5,8 +5,8 @@ import com.limefamily.recommend.model.User;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.Header;
-import retrofit2.http.POST;
 
 /**
  * Created by liuhao on 2018/4/10.
@@ -14,7 +14,7 @@ import retrofit2.http.POST;
 
 public interface UserService {
 
-    @POST("/user/view")
+    @GET("/user/view")
     @FormUrlEncoded
     Call<User> view(@Header("Authorization") String authorization,@Field("id") int id);
 }
